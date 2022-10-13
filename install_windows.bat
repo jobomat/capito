41388d66d05084a69482cc07710bf470457c8fb4
Jochen Bomm <herrbomm@googlemail.com>
2022-10-13 11:24:16 +0200

    skippable download in install_windows.bat

--- install_windows.bat
+++ install_windows.bat
@@ -1,5 +1,5 @@
 pushd "%~dp0"
-
+@ECHO off
 SET VENVS=venvs
 SET SYSTEM_VENV=system
 SET MAYA_VENV=maya
@@ -15,19 +15,21 @@ py -3 -m venv %VENVS%\%MAYA_VENV%
 REM Set the CAPITO_BASE_DIR environment variable to current directory.
 setx CAPITO_BASE_DIR %CD%
 
-REM Execute the pytho script to download 3rd party programs (ffmpeg)
-.\%VENVS%\%SYSTEM_VENV%\Scripts\python .\capito\get_vendor_apps.py
+%SystemRoot%\System32\choice.exe /C YN /N /M "Download FFMPEG now? [Y/N]"
+if not errorlevel 1 goto DOWNLOAD
+if errorlevel 2 goto SKIP
 
+:DOWNLOAD
+REM Execute the python script to download 3rd party programs (ffmpeg)
+.\%VENVS%\%SYSTEM_VENV%\Scripts\python .\capito\get_vendor_apps.py
 REM Clean up downloaded packed files
 del vendor\ffmpeg
-
 REM set FFMPEG environment variable to ffmpeg.exe
 setx FFMPEG "%CD%\vendor\%FFMPEG_DIR%\bin\ffmpeg.exe"
-
 REM add ffmpeg dir to users PATH (Not really necessary but convenient for plumbum import)
 for /f "usebackq tokens=2,*" %%A in (`reg query HKCU\Environment /v PATH`) do set my_user_path=%%B
 setx PATH "%CD%\vendor\%FFMPEG_DIR%\bin\;%my_user_path%"
-
+:SKIP
 @pause
 
 popd
