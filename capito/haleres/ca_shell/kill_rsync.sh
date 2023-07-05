c9da7668b0f7efe50933f2cd141a1cce87441261
Jochen Bomm <herrbomm@googlemail.com>
2023-07-05 09:22:20 +0200

    rsync kill script with exit condition

--- capito/haleres/ca_shell/kill_rsync.sh
+++ capito/haleres/ca_shell/kill_rsync.sh
@@ -5,7 +5,12 @@ ADDITIONAL_PATTERN=$3
 
 # get all rsync pids with $ADDITIONAL_PATTERN, $SHARE and $JOBNAME in the command-listing
 # $ADDITIONAL_PATTERN could be e.g. "files_to_push" to kill only push actions...
-RSYNC_PIDS=$(ps -eF --no-headers -p $(pidof rsync) |
+ALL_RSYNC_PIDS=$(pidof rsync)
+if [ -n "$ALL_RSYNC_PIDS" ]; then
+    exit 1
+fi
+
+RSYNC_PIDS=$(ps -eF --no-headers -p $ALL_RSYNC_PIDS |
     grep "rsync.*$ADDITIONAL_PATTERN.*$SHARE.*$JOBNAME" |
     awk '{ print $1 }')
     
