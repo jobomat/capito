9de5984089f34af3abe6b6a7e4532ab9feae85e3
Jochen Bomm <herrbomm@googlemail.com>
2023-07-05 15:02:04 +0200

    added share to path in kill_push

--- capito/haleres/ca_shell/kill_push.sh
+++ capito/haleres/ca_shell/kill_push.sh
@@ -5,8 +5,8 @@ source $SCRIPT_PATH/settings.sh
 SHARE=$1
 JOBNAME=$2
 
-IPC_DIR="${MOUNT_POINT}/hlrs/${JOBNAME}/ipc/"
+IPC_DIR="${MOUNT_POINT}/${SHARE}/hlrs/${JOBNAME}/ipc/"
 NOW=$( date '+%F_%H-%M-%S' )
 
-$SCRIPT_PATH/kill_rsync.sh $1 $2 "files_to_push" > "${IPC_DIR}/rsync/kill_${NOW}.log" 2>&1
+$SCRIPT_PATH/kill_rsync.sh $SHARE $JOBNAME "files_to_push" > "${IPC_DIR}/rsync/kill_${NOW}.log" 2>&1
 touch "${IPC_DIR}/status/PUSH_ABORTED"
