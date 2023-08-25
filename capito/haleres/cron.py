a328a6da758cac8226c2749b8bbaffb2b38a9200
Jochen Bomm <herrbomm@googlemail.com>
2023-08-25 12:23:22 +0200

    cron test 3

--- capito/haleres/cron.py
+++ capito/haleres/cron.py
@@ -5,14 +5,12 @@ import sys
 capito_path = sys.argv[1]
 haleres_settings_file = sys.argv[2]
 
-print(capito_path, haleres_settings_file)
+sys.path.append(capito_path)
+from capito.haleres.settings import Settings
+from capito.haleres.job import JobProvider
 
-# sys.path.append(str(capito_path))
 
-# from capito.haleres.settings import Settings
-# from capito.haleres.job import JobProvider
+haleres_settings = Settings(haleres_settings_file)
+jp = JobProvider(haleres_settings)
 
-# haleres_settings = Settings(haleres_settings_file)
-# jp = JobProvider(haleres_settings)
-
-# print(jp.jobs)
+print(jp.jobs)
