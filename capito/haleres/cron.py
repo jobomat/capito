9829b18e18766238d5f554c2fc1a3a5b7defcb38
Jochen Bomm <herrbomm@googlemail.com>
2023-08-25 12:03:46 +0200

    cron test 1

--- capito/haleres/cron.py
+++ capito/haleres/cron.py
@@ -3,5 +3,13 @@ from pathlib import Path
 import sys
 
 
-this_file_path = inspect.getfile(lambda: None)
-print(this_file_path)
+capito_path = Path(inspect.getfile(lambda: None)).parent.parent.parent
+sys.path.append(str(capito_path))
+
+from capito.haleres.settings import Settings
+from capito.haleres.job import JobProvider
+
+s = Settings("/mnt/cg/pipeline/hlrs/settings.json")
+jp = JobProvider(s)
+
+print(jp.jobs)
